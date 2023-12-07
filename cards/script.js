130196c05455324af17b9afb13d638dad5b4d1a9
MohanapriyaThirumurugan <mohanapriyathirumurugan17@gmail.com>
2023-12-07 10:35:31 +0530

    new

---
+++ cards/script.js
@@ -0,0 +1,46 @@
+const val=document.createElement("h1")
+val.setAttribute("class","class")
+document.body.append(val)
+setTimeout(()=>{
+  val.innerText="10"
+  setTimeout(()=>{
+    val.innerText="9"
+    setTimeout(()=>{
+      val.innerText="8"
+      setTimeout(()=>{
+        val.innerText="7"
+        setTimeout(()=>{
+          val.innerText="6"
+          setTimeout(()=>{
+            val.innerText="5"
+            setTimeout(()=>{
+              val.innerText="4"
+              setTimeout(()=>{
+                val.innerText="3"
+                setTimeout(()=>{
+                  val.innerText="2"
+                  setTimeout(()=>{
+                    val.innerText="1"
+                    setTimeout(()=>{
+                      val.innerText="HAPPY INDEPENCEDAY"
+                    },1000)
+                    
+                  },1000)
+                  
+                },1000)
+                
+              },1000)
+              
+            },1000)
+            
+          },1000)
+          
+        },1000)
+        
+      },1000)
+      
+    },1000)
+    
+  },1000)
+  
+},1000)
